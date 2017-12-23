ce01319ba6c7313a77a100949db94aea95a42f05
Devin Brown <devin.g.brown@gmail.com>
2017-12-23 07:08:47 -0700

    add simple validation for ex.

--- applications/core/appCommand.js
+++ applications/core/appCommand.js
@@ -1,10 +1,13 @@
 import * as AppEvent from './appEvent';
+import {Todo} from "./appValidator";
 
 export function process(commands, store) {
     commands.forEach((command) => {
         switch (command.type) {
             case 'CreateTodo':
-                AppEvent.create('CreateTodo', command.data, store);
+                if (Todo.isValid(command.data)) {
+                    AppEvent.create('CreateTodo', command.data, store);
+                }
                 break;
             default:
                 console.log('unknown AppCommand received')
---
+++ applications/core/appValidator.js
@@ -0,0 +1,5 @@
+export const Todo = {
+    isValid: (data) => {
+        return data.text.length > 0;
+    }
+};
